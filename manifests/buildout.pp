d1aa1a85dc9690658de603da7a3fad441a3517c3
Bjørn Øivind Bjørnsen <Bjorn.Oivind.Bjornsen@jotron.com>
2014-06-04 12:03:29 +0200

    manifests: add Arch Linux support and other fixes.
    This adds Arch Linux support to the module, fixes some inconsistent
    indentation and white space damage, adds a new parameter to avoid
    hardcoding the base directory to somewhere under /srv, fixes the
    environment when running the buildout and makes sure that supervisord
    only runs once (although it depends on the supervisord socket being
    placed in var/supervisor.sock under the buildout directory.)

--- manifests/buildout.pp
+++ manifests/buildout.pp
@@ -1,8 +1,10 @@
-class plone::buildout($buildout_dir, $repo_url, $user, $server_config){
+class plone::buildout($base_dir, $buildout_dir, $repo_url, $user, $group, $server_config) {
+
+    package { 'git': ensure => "installed" }
 
     file { "${buildout_dir}/var":
         ensure => directory,
-	require => Exec['clone_buildout'],
+        require => Exec['clone_buildout'],
         owner => $user,
     }
 
@@ -20,53 +22,65 @@ class plone::buildout($buildout_dir, $repo_url, $user, $server_config){
 
     file { "${buildout_dir}/buildout-cache/downloads":
         ensure => "directory",
-	require => File["$buildout_dir/buildout-cache"],
+        require => File["$buildout_dir/buildout-cache"],
         owner => $user,
-    }
-
-    # srv needs permissions of hte user to clone appropriately
-    file { "/srv":
-        mode  => '0664',
-        owner => $user,
-        group => 'www',
     }
 
     exec { "create_virtualenv":
-        command => "/usr/bin/virtualenv --no-setuptools ${buildout_dir}",
+        command => $operatingsystem ? {
+            Archlinux => "/usr/bin/virtualenv2 --no-setuptools ${buildout_dir}",
+            default => "/usr/bin/virtualenv --no-setuptools ${buildout_dir}",
+        },
         creates => "${buildout_dir}/bin/python",
         user =>	$user,
         require => Exec['clone_buildout'],
     }
 
+    file { "${base_dir}":
+        ensure => "directory",
+        owner => $user,
+        group => $group,
+    }
+
     exec{ "clone_buildout":
         creates => "${buildout_dir}",
         command => "/usr/bin/git clone ${repo_url} ${buildout_dir}",
-        cwd => '/srv',
-        require => [ Package['git'], 
+        cwd => "${base_dir}",
+        require => [ Package['git'],
                      User[$user],
-                     File['/srv'], ],
+                     File[$base_dir], ],
         user =>	$user,
-   }
-
-   exec { 'run_bootstrap':
-      creates => "${buildout_dir}/bin/buildout",
-      cwd => "${buildout_dir}",
-      command => "${buildout_dir}/bin/python ${buildout_dir}/bootstrap.py",
-      subscribe => Exec['clone_buildout'],
-      require => Exec['create_virtualenv'],      
-      user => $user,
     }
 
-   exec { 'initial_buildout':
-      cwd => "${buildout_dir}",
-      command => "${buildout_dir}/bin/buildout -c ${buildout_dir}/${server_config}",
-      subscribe => Exec['run_bootstrap'],
-      user => $user,
-   }
+    exec { 'run_bootstrap':
+        creates => "${buildout_dir}/bin/buildout",
+        cwd => "${buildout_dir}",
+        command => "${buildout_dir}/bin/python ${buildout_dir}/bootstrap.py -c ${server_config}",
+        subscribe => Exec['clone_buildout'],
+        require => Exec['create_virtualenv'],
+        user => $user,
+    }
 
-   exec { 'start_supervisor':
-      cwd => "${buildout_dir}",
-      command => "${buildout_dir}/bin/supervisord",
-      subscribe => Exec['initial_buildout'],
-   }
+    exec { 'initial_buildout':
+        cwd => "${buildout_dir}",
+        command => "${buildout_dir}/bin/buildout -c ${buildout_dir}/${server_config}",
+        # buildout may depend on finding the correct user and group in the environment in some cases.
+        environment => [ "USER=$user",
+                         "GROUP=$group",
+                       ],
+        subscribe => Exec['run_bootstrap'],
+        # this operation may very well take a fair amount of time.
+        timeout => 3600,
+        user => $user,
+    }
+
+    exec { 'start_supervisor':
+        creates => "${buildout_dir}/var/supervisor.sock",
+        cwd => "${buildout_dir}",
+        command => "${buildout_dir}/bin/supervisord",
+        subscribe => Exec['initial_buildout'],
+        user => $user,
+    }
 }
+
+# vim: ts=4 shiftwidth=4 expandtab:
